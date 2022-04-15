8ca783ae5383c61cb6a2c01eb66d59717bb0292b
aditya-taori <aditya-taori>
2022-04-15 18:41:06 +0000

    Lab_2_3_4_implementation

--- project3-mlops/Labs/03-Lab.py
+++ project3-mlops/Labs/03-Lab.py
@@ -58,9 +58,15 @@ print("Created directory `{}` to house the project files.".format(path))
 #  TODO
 dbutils.fs.put(path + "MLproject", 
 '''
-
-  FILL_IN
-
+name: Lab-03
+conda_env: conda.yaml
+entry_points:
+  main:
+    parameters:
+      data_path: {type: str, default: "/dbfs/mnt/training/airbnb/sf-listings/airbnb-cleaned-mlflow.csv"}
+      bootstrap: {type: bool, default: True}
+      min_impurity_decrease: {type: float, default: 0}
+    command: "python train.py --data_path {data_path} --bootstrap {bootstrap} --min_impurity_decrease {min_impurity_decrease}"
 '''.strip())
 
 # COMMAND ----------
@@ -82,9 +88,16 @@ dbutils.fs.put(path + "MLproject",
 #  TODO
 dbutils.fs.put(path + "conda.yaml", 
 '''
-
-  FILL_IN
-
+name: Lesson-03
+channels:
+  - defaults
+dependencies:
+  - cloudpickle={1.6.0}
+  - numpy={1.19.2}
+  - pandas={1.2.4}
+  - scikit-learn={0.24.1}
+  - pip:
+    - mlflow=={1.20.2}
 '''.strip())
 
 # COMMAND ----------
@@ -99,9 +112,38 @@ dbutils.fs.put(path + "conda.yaml",
 #  TODO
 dbutils.fs.put(path + "train.py", 
 '''
-
-  FILL_IN
-  
+import click
+import mlflow.sklearn
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+@click.command()
+@click.option("--data_path", default="/dbfs/mnt/training/airbnb/sf-listings/airbnb-cleaned-mlflow.csv", type=str)
+@click.option("--bootstrap", default=True, type=bool)
+@click.option("--min_impurity_decrease", default=0, type=float)
+def mlflow_rf(data_path, bootstrap, min_impurity_decrease):
+  with mlflow.start_run() as run:
+    # Import the data
+    df = pd.read_csv(data_path)
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(["price"], axis=1), df[["price"]].values.ravel(), random_state=42)
+    
+    # Create model, train it, and create predictions
+    rf = RandomForestRegressor(bootstrap=bootstrap, min_impurity_decrease=min_impurity_decrease)
+    rf.fit(X_train, y_train)
+    predictions = rf.predict(X_test)
+    # Log model
+    mlflow.sklearn.log_model(rf, "random-forest-model")
+    
+    # Log params
+    mlflow.log_param("bootstrap", bootstrap)
+    mlflow.log_param("min_impurity_decrease", min_impurity_decrease)
+    # Log metrics
+    mlflow.log_metric("mse", mean_squared_error(y_test, predictions))
+    mlflow.log_metric("mae", mean_absolute_error(y_test, predictions))  
+    mlflow.log_metric("r2", r2_score(y_test, predictions))  
+if __name__ == "__main__":
+  mlflow_rf() # Note that this does not need arguments thanks to click
 '''.strip())
 
 # COMMAND ----------
--- project3-mlops/Labs/04-Lab-Runnable.py
+++ project3-mlops/Labs/04-Lab-Runnable.py
@@ -1,29 +1,74 @@
 # Databricks notebook source
 # TODO
-Create 3 widgets for parameter passing into the notebook:
-  - n_estimators with a default of 100
-  - learning_rate with a default of .1
-  - max_depth with a default of 1 
-Note that only strings can be used for widgets
+#Create 3 widgets for parameter passing into the notebook:
+#  - n_estimators with a default of 100
+#  - learning_rate with a default of .1
+#  - max_depth with a default of 1 
+#Note that only strings can be used for widgets
+
+dbutils.widgets.text("n_estimators", "100")
+dbutils.widgets.text("learning_rate", ".1")
+dbutils.widgets.text("max_depth", "1")
 
 # COMMAND ----------
 
 # TODO
-Read from the widgets to create 3 variables.  Be sure to cast the values to numeric types
-n_estimators = FILL_IN
-learning_rate = FILL_IN
-max_depth = FILL_IN
+#Read from the widgets to create 3 variables.  Be sure to cast the values to numeric types
+n_estimators = int(dbutils.widgets.get("n_estimators"))    # Cast to Int
+learning_rate = float(dbutils.widgets.get("learning_rate"))          # Cast to Int
+max_depth = int(dbutils.widgets.get("max_depth")) 
 
 # COMMAND ----------
 
 # TODO
-Train and log the results from a model.  Try using Gradient Boosted Trees
-https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingRegressor.html#sklearn.ensemble.GradientBoostingRegressor
+#Train and log the results from a model.  Try using Gradient Boosted Trees
+#https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingRegressor.html#sklearn.ensemble.GradientBoostingRegressor
+
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+data_path = "/dbfs/mnt/training/airbnb/sf-listings/airbnb-cleaned-mlflow.csv"
+with mlflow.start_run() as run:
+  # Import the data
+    df = pd.read_csv(data_path)
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(["price"], axis=1), df[["price"]].values.ravel(), random_state=42)
+
+    # Create model, train it, and create predictions
+    gbf = GradientBoostingRegressor(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate)
+    gbf.fit(X_train, y_train)
+    predictions = gbf.predict(X_test)
+
+    # Log model
+    model_path = "Gradient Boosting Regressor"
+    mlflow.sklearn.log_model(gbf, model_path)
+
+    # Log params
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("learning_rate", learning_rate)
+
+    # Log metrics
+    mlflow.log_metric("mse", mean_squared_error(y_test, predictions))
+    mlflow.log_metric("mae", mean_absolute_error(y_test, predictions))  
+    mlflow.log_metric("r2", r2_score(y_test, predictions)) 
+
+    #artifactURI = mlflow.get_artifact_uri()
+    model_output_path = "runs:/" + run.info.run_id + "/" + model_path
 
 # COMMAND ----------
 
 # TODO
-Report the model output path to the parent notebook
+# Report the results back to the parent notebook
+import json
+
+dbutils.notebook.exit(json.dumps({
+  "status": "OK",
+  "model_output_path": model_output_path, #.replace("dbfs:", "/dbfs")
+  "data_path": data_path
+}))
 
 
 # COMMAND ----------
